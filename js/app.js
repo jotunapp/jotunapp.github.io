587b19216072bae481e650e7168ab8c2dd25cff2
Prashanth Seshadri <sp.1990@gmail.com>
2014-11-28 09:45:27 +0530

    Pushing the initial set of changes
    Pushing the changes

---
+++ js/app.js
@@ -0,0 +1,65 @@
+var app = {
+  system: 'sysA'
+};
+
+var bindSystemChange = function () {
+  "use strict";
+  $('[name=system]').on('change', function () {
+    var id = $(this).attr('id');
+    $(document).trigger('syschanged', [id]);
+  });
+};
+
+var bindPopovers = function () {
+  "use strict";
+  $("#lifetime")
+    .popover({
+      content: "Eg. value is 30",
+      trigger: 'focus',
+      placement: 'top'
+    });
+};
+
+var compute = function () {
+  "use strict";
+  
+  var data = {
+    labels: ["Initial total cost of System/sq.m", "Total Cost/sq.m", "Project Cost for full size(over lifetime)", "Life cycle cost per year", "Life cycle cost for 5yrs"],
+    datasets: [
+      {
+        label: "System A",
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        data: [38.68, 38.7, 386784, 12983, 64464]
+        },
+      {
+        label: "System B",
+        fillColor: "rgba(151,187,205,0.5)",
+        strokeColor: "rgba(151,187,205,0.8)",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        data: [27.50, 46.5, 465273, 23264, 116318]
+        }
+    ]
+  };
+
+  var ctx = $("#costSummary").get(0).getContext("2d");
+  var chart = new Chart(ctx).Bar(data, {
+    tooltipTitleFontSize : 0
+  });
+  $('#costModal').modal({
+    show: true
+  });
+};
+
+$(document).ready(function () {
+  "use strict";
+  bindSystemChange();
+  bindPopovers();
+  $(document).on('syschanged', function (event, id) {
+    app.system = id;
+    console.log(app.system);
+  });
+});
---
+++ js/data.js
@@ -0,0 +1,49 @@
+var curSys = '';
+var costData = {
+    sysA: [
+        {
+            ppl: 13,
+            requireDFT: 600,
+            perVS: 98,
+            factor: 10
+        }, {
+            ppl: 13,
+            requireDFT: 600,
+            perVS: 98,
+            factor: 10
+        }, {
+            ppl: 0,
+            requireDFT: 0,
+            perVS: 1,
+            factor: 10
+        }, {
+            ppl: 0,
+            requireDFT: 0,
+            perVS: 1,
+            factor: 10
+        }
+    ],
+    sysB: [
+        {
+            ppl: 12,
+            requireDFT: 50,
+            perVS: 72,
+            factor: 10
+        }, {
+            ppl: 5.2,
+            requireDFT: 130,
+            perVS: 72,
+            factor: 10
+        }, {
+            ppl: 7.3,
+            requireDFT: 60,
+            perVS: 63,
+            factor: 10
+        }, {
+            ppl: 0,
+            requireDFT: 0,
+            perVS: 1,
+            factor: 10
+        }
+    ]
+};
